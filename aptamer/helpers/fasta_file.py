aa2bbb33985279656fbb4e718a4548600de3c248
Chris Ortman <chris-ortman@uiowa.edu>
2019-07-03 19:49:06 -0500

    Refactor how rna output is processed

--- aptamer/helpers/fasta_file.py
+++ aptamer/helpers/fasta_file.py
@@ -16,33 +16,42 @@ class FastaFile(object):
         self.pass_options = ''
         self.vienna_version = 2
 
+    def get_cluster_size(self, record):
+        cluster_size_re = self.cluster_size_re
+        cluster_size = 1
+        try:
+            cluster_size = cluster_size_re.search(record.description)
+            cluster_size = cluster_size.group(1)
+        except AttributeError:
+            pass
+            # print('Not able to find cluster size. Setting to 1.')
+
+        if cluster_size is None:
+            cluster_size = 1
+
+        return cluster_size
+
+    def get_sequence(self, record):
+        prefix = self.prefix
+        suffix = self.suffix
+        sequence = '%s%s%s'.replace('T', 'U') % (
+            prefix, str(record.seq), suffix
+        )
+        return sequence
+
     def __iter__(self):
         """Process input file as fasta. Populate RNASequence (graph vertex)
         objects.
         """
 
         in_fh = open(self.input_file_name, 'r')
-        cluster_size_re = self.cluster_size_re
-        prefix = self.prefix
-        suffix = self.suffix
         run_mfold = self.run_mfold
         pass_options = self.pass_options
         vienna_version = self.vienna_version
 
         for record in SeqIO.parse(in_fh, 'fasta'):
-            sequence = '%s%s%s'.replace('T', 'U') % (
-                prefix, str(record.seq), suffix
-            )
-            cluster_size = 1
-            try:
-                cluster_size = cluster_size_re.search(record.description)
-                cluster_size = cluster_size.group(1)
-            except AttributeError:
-                pass
-                # print('Not able to find cluster size. Setting to 1.')
-
-            if cluster_size is None:
-                cluster_size = 1
+            sequence = self.get_sequence(record)
+            cluster_size = self.get_cluster_size(record)
 
             # find structure
             curr_seq = RNASequence(record.id, cluster_size, sequence)
@@ -52,44 +61,57 @@ class FastaFile(object):
             else:
 
                 rnafold_out = run_rnafold(sequence, vienna_version, pass_options)
-                rnafold_out = rnafold_out.split('\n')
-                try:
-                    curr_seq.structure, curr_seq.free_energy = (
-                        rnafold_out[1].split(' (')
-                    )
-                except (ValueError, IndexError):
-                    print('Error running RNAfold:\n%s\nExiting.' % rnafold_out)
-                    sys.exit(1)
+                rnafold_out = RNAFoldOutput(rnafold_out)
 
-                print('%s\n' % rnafold_out)
-                try:
-                    curr_seq.free_energy = abs(
-                        float(curr_seq.free_energy.replace(')', ''))
-                    )
-                    curr_seq.ensemble_free_energy = abs(
-                        float(rnafold_out[2].split('[')[1].replace(']', ''))
-                    )
-                    curr_seq.ensemble_probability = abs(float(
-                        rnafold_out[4].split(';')[0].replace(
-                            ' frequency of mfe structure in ensemble ', ''
-                        )
-                    ))
-                    curr_seq.ensemble_diversity = abs(float(
-                        rnafold_out[4].split(';')[1].replace(
-                            ' ensemble diversity ', ''
-                        )
-                    ))
-                except IndexError:
-                    print(
-                        'Error parsing RNAfold output. '
-                        '(Couldn\'t find statistics.) Please check '
-                        'RNAfold options.'
-                    )
-                    sys.exit(1)
+                curr_seq.structure = rnafold_out.structure
+                curr_seq.free_energy = rnafold_out.free_energy
+                curr_seq.free_energy = rnafold_out.free_energy
+                curr_seq.ensemble_free_energy = rnafold_out.ensemble_free_energy
+                curr_seq.ensemble_probability = rnafold_out.ensemble_probability
+                curr_seq.ensemble_diversity = rnafold_out.ensemble_diversity
 
             (yield curr_seq)
 
         in_fh.close()
+
+class RNAFoldOutput(object):
+
+    def __init__(self, rnafold_out):
+        rnafold_out = rnafold_out.split('\n')
+        try:
+            structure, free_energy = (
+                rnafold_out[1].split(' (')
+            )
+            self.structure = structure
+            self.free_energy = abs( float(free_energy.replace(')', '')))
+        except (ValueError, IndexError):
+            print('Error running RNAfold:\n%s\nExiting.' % rnafold_out)
+            sys.exit(1)
+
+        print('%s\n' % rnafold_out)
+
+        try:
+            self.ensemble_free_energy = abs(
+                float(rnafold_out[2].split('[')[1].replace(']', ''))
+            )
+            self.ensemble_probability = abs(float(
+                rnafold_out[4].split(';')[0].replace(
+                    ' frequency of mfe structure in ensemble ', ''
+                )
+            ))
+            self.ensemble_diversity = abs(float(
+                rnafold_out[4].split(';')[1].replace(
+                    ' ensemble diversity ', ''
+                )
+            ))
+        except IndexError:
+            print(
+                'Error parsing RNAfold output. '
+                '(Couldn\'t find statistics.) Please check '
+                'RNAfold options.'
+            )
+            sys.exit(1)
+        pass
 
 def run_rnafold(seq, vienna_version, pass_options):
     print('##################')
