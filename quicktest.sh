06e14febd2cc86f7af911edb69d367def947cf6a
Chris Ortman <chris-ortman@uiowa.edu>
2019-07-15 13:13:56 -0500

    Add calculate stats to quickt test

--- quicktest.sh
+++ quicktest.sh
@@ -1,4 +1,4 @@
-./predict-structures samples/Trunc_Test.fa
+./predict-structures samples/Trunc_Test.fa --calculate_stats
 ./create-graph data/Trunc_Test/Trunc_Test.fa.struct.fa
 icdiff data/Trunc_Test/golden_master.xgmml data/Trunc_Test/Trunc_Test.fa.struct.fa.xgmml
 
