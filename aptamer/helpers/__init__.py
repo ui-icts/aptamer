7370c3a10f0ddc9a4de28ab77c0fffe943aee0ca
Chris Ortman <chris-ortman@uiowa.edu>
2019-07-15 13:49:18 -0500

    Fix typo in module

--- aptamer/helpers/__init__.py
+++ aptamer/helpers/__init__.py
@@ -3,7 +3,7 @@ import sys
 __all__ = [
         'functions',
         'rna_sequence',
-        'rna_sequnce_pair',
+        'rna_sequence_pair',
         'fasta_struct_file',
         'fasta_file',
        ]
