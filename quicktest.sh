6a87083c00590f7aa5680ec9b4ed6861e2a8477b
Chris Ortman <chris-ortman@uiowa.edu>
2019-07-17 08:34:02 -0500

    Robustness for quick test script

--- quicktest.sh
+++ quicktest.sh
@@ -1,4 +1,17 @@
-./predict-structures samples/Trunc_Test.fa --calculate_stats
-./create-graph data/Trunc_Test/Trunc_Test.fa.struct.fa
-icdiff data/Trunc_Test/golden_master.xgmml data/Trunc_Test/Trunc_Test.fa.struct.fa.xgmml
+set -e
+
+readonly graph_file=data/Trunc_Test/Trunc_test.fa.struct.fa.xgmml
+
+echo "Running predict structures"
+./predict-structures samples/Trunc_Test.fa --calculate_stats > /dev/null
+
+echo "Comparing create graph output (no seed)"
+rm -f $graph_file
+./create-graph data/Trunc_Test/Trunc_Test.fa.struct.fa > /dev/null
+icdiff test/create_graph_golden_master_no_seed.xgmml $graph_file
+
+echo "Comparing create graph output (seed)"
+rm -f $graph_file
+./create-graph data/Trunc_Test/Trunc_Test.fa.struct.fa --seed > /dev/null
+icdiff test/create_graph_golden_master_seed.xgmml $graph_file
 
